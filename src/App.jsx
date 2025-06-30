b234690652aea52528bf1949c49148696dce477c
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-30 22:33:40 +0300

    feat(setup): T01: Project Setup#1
    All necessary dependencies and configurations have been installed and set up, including Vite, React, Redux, Redux Thunk, React Router v5, Tailwind CSS, Axios, React-Toastify, and Lucide React icons. Tailwind CSS configuration files (tailwind.config.cjs, postcss.config.cjs) have been created and updated.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-5xl font-bold text-blue-600">@elfctn e-commerce</h1>
+    </div>
+  );
+}
+
+export default App;
