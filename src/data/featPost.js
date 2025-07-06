a7e20ff5c02e35f40375344514343ad681635a24
Elif Cetin <elifcetin.dev@gmail.com>
2025-07-07 00:40:01 +0300

    feat(data): add featPost, shopProducts, and team data files

---
+++ src/data/featPost.js
@@ -0,0 +1,26 @@
+const featPost = [
+  {
+    id: 1,
+    imageUrl: "/grafik1.png",
+    category: "Popular Blog Cards ",
+    title: "Graphic Design",
+    description:
+      "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+    sales: 15,
+    price: 6.48,
+    originalPrice: 16.48,
+  },
+  {
+    id: 2,
+    imageUrl: "/grafik2.png",
+    category: "Popular Blog Cards",
+    title: "Graphic Design",
+    description:
+      "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+    sales: 15,
+    price: 6.48,
+    originalPrice: 16.48,
+  },
+];
+
+export default featPost;
---
+++ src/data/team.js
@@ -0,0 +1,44 @@
+export const teamMembers = [
+  {
+    id: 1,
+    name: "Elif Cetin",
+    title: "Project Lead & Developer",
+    imageUrl: "/elf.png",
+    socials: { facebook: "#", instagram: "#", twitter: "#" },
+  },
+  {
+    id: 2,
+    name: "Enis Gayretli",
+    title: "Java Developer",
+    imageUrl: "/ens.png",
+    socials: { facebook: "#", instagram: "#", twitter: "#" },
+  },
+  {
+    id: 3,
+    name: "Emre Sahiner",
+    title: "UI/UX Designer",
+    imageUrl: "/emr.png",
+    socials: { facebook: "#", instagram: "#", twitter: "#" },
+  },
+  {
+    id: 4,
+    name: "Ceren Ayder",
+    title: "Human Resources",
+    imageUrl: "/crn.png",
+    socials: { facebook: "#", instagram: "#", twitter: "#" },
+  },
+  {
+    id: 5,
+    name: "Yalcin Kaya",
+    title: "Project Manager",
+    imageUrl: "/ylcn.png",
+    socials: { facebook: "#", instagram: "#", twitter: "#" },
+  },
+  {
+    id: 6,
+    name: "Inci Bayram",
+    title: "Content Specialist",
+    imageUrl: "/inc.png",
+    socials: { facebook: "#", instagram: "#", twitter: "#" },
+  },
+];
